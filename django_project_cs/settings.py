8239bcbc1d3b14d678e267722002d7ef27fb6e70
Raghav <raghavdoon@gmail.com>
2021-07-02 23:25:55 +0530

    Environment Variables

--- django_project_cs/settings.py
+++ django_project_cs/settings.py
@@ -9,12 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+i%v=1*+g(ylr&@7*9m&*c-w%=i+%5c$=amv*xr!n(_eq+o60h'
+# SECRET_KEY = 'django-insecure-+i%v=1*+g(ylr&@7*9m&*c-w%=i+%5c$=amv*xr!n(_eq+o60h'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myawesomeblogappraghav.herokuapp.com']
 
 
 # Application definition
