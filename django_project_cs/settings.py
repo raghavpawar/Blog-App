a135a43535fdbf695a487aca2375283fa2897a49
Raghav <raghavdoon@gmail.com>
2021-07-03 19:25:14 +0530

    AWS settings

--- django_project_cs/settings.py
+++ django_project_cs/settings.py
@@ -137,14 +137,17 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_FILE_OVERWRITE = False
+# AWS_S3_FILE_OVERWRITE = False
 # AWS_DEFAULT_ACL = None
 
-AWS_S3_REGION_NAME = 'ap-south-1'
-AWS_S3_ADDRESSING_STYLE = 'virtual'
+# AWS_S3_REGION_NAME = 'ap-south-1'
+# AWS_S3_ADDRESSING_STYLE = 'virtual'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'django_project_cs.storage_backends.MediaStorage'  # <-- here is where we reference it
 
 django_heroku.settings(locals())
