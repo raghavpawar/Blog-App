db32e3b2fcb7107801b838d3a8656a23d8cb1d4c
Raghav <raghavdoon@gmail.com>
2021-07-03 19:10:55 +0530

    bucket name

--- django_project_cs/settings.py
+++ django_project_cs/settings.py
@@ -145,5 +145,5 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'ap-south-1'
 AWS_S3_ADDRESSING_STYLE = 'virtual'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 django_heroku.settings(locals())
